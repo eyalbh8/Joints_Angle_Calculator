c3e6eb91b136c8db72054d941e41cc58c9e1c8ae
eyal ben haim <eyalbh8@gmail.com>
2022-02-24 15:46:18 +0200

    add test and cleanups

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-from math import radians
 import cv2
 import mediapipe as mp
 import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-def calculate_angle(first,mid,end):
+
+def calculate_angle(first, mid, end):
     first = np.array(first)
     mid = np.array(mid)
     end = np.array(end)
 
-    radians = np.arctan2(end[1]-mid[1], end[0]-mid[0]) - np.arctan2(first[1]-mid[1], first[0]-mid[0])
-    angle = np.abs(radians*180/np.pi)
+    radians = np.arctan2(end[1] - mid[1], end[0] - mid[0]) - np.arctan2(first[1] - mid[1], first[0] - mid[0])
+    angle = np.abs(radians * 180 / np.pi)
 
     if angle > 180.0:
         angle = 360 - angle
-    
+
     return angle
+
 
 cap = cv2.VideoCapture(0)
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         ret, frame = cap.read()
-        
+
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
         results = pose.process(image)
@@ -33,35 +35,37 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
             landmarks = results.pose_landmarks.landmark
 
-            Left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, 
+            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
                              landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
 
-            Left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, 
+            left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
                           landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
 
-            Left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, 
+            left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
                           landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            
-            Elbow_angle = calculate_angle(Left_shoulder,Left_elbow,Left_wrist)
 
-            cv2.putText(image, str(Elbow_angle),
-                        tuple(np.multiply(Left_elbow,[640,480]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
+            elbow_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+            print(f"shoulder: {left_shoulder} \n elbow: {left_elbow} \n wrist: {left_wrist}")
+            print(elbow_angle)
 
-            Right_foot_index = [landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x, 
+            cv2.putText(image, str(elbow_angle),
+                        tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            right_foot_index = [landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x,
                                 landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].y]
-            
-            Right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x, 
+
+            right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
                            landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
 
-            Right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x, 
+            right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
                           landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
 
-            Ankle_angle = calculate_angle(Right_foot_index,Right_ankle,Right_knee)
+            Ankle_angle = calculate_angle(right_foot_index, right_ankle, right_knee)
 
             cv2.putText(image, str(Ankle_angle),
-                        tuple(np.multiply(Right_ankle,[640,480]).astype(int)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
+                        tuple(np.multiply(right_ankle, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
         except:
             pass
---
+++ tests/test_angles.py
@@ -0,0 +1,14 @@
+from main import calculate_angle
+
+joints = {
+    "shoulder": [0.31897982954978943, 0.2254987210035324],
+    "elbow": [0.7195838689804077, 0.642013669013977],
+    "wrist": [0.8107253313064575, 0.3998505771160126],
+    "elbow_angle": 64.50900905642266
+}
+
+
+def test_angle_calculating():
+    angle = calculate_angle(joints["shoulder"], joints["elbow"], joints["wrist"])
+
+    assert angle == joints["elbow_angle"]
